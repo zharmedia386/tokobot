ef80394afaf46df5dff1a2a7f32dfe630b5f2300
M Azhar Alauddin <80077273+zharmedia386@users.noreply.github.com>
2022-08-20 09:04:49 +0700

    form purchase

--- resources/views/app/purchase_form.blade.php
+++ resources/views/app/purchase_form.blade.php
@@ -5,59 +5,43 @@
 <div class="card">
     <div class="card-header d-flex justify-content-between">
         <div class="header-title">
-            <h4 class="card-title">Input</h4>
+            <h4 class="card-title">Purchase Invoice</h4>
         </div>
     </div>
     <div class="card-body">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vulputate, ex ac venenatis mollis, diam nibh finibus leo</p>
         <form>
             <div class="form-group">
-                <label class="form-label" for="exampleInputText1">Input Text </label>
-                <input type="text" class="form-control" id="exampleInputText1" value="Mark Jhon" placeholder="Enter Name" />
+                <label class="form-label" for="exampleInputText1">Nomor Transaksi</label>
+                <input type="text" class="form-control" id="exampleInputText1" placeholder="Nomor Transaksi.." />
             </div>
             <div class="form-group">
-                <label class="form-label" for="exampleInputEmail3">Email Input</label>
-                <input type="email" class="form-control" id="exampleInputEmail3" value="markjhon@gmail.com" placeholder="Enter Email" />
+                <label class="form-label" for="exampleInputdate">Tanggal Transaksi</label>
+                <input type="date" class="form-control" id="exampleInputdate" />
             </div>
             <div class="form-group">
-                <label class="form-label" for="exampleInputurl">Url Input</label>
-                <input type="url" class="form-control" id="exampleInputurl" value="https://getbootstrap.com" placeholder="Enter Url" />
+                <label class="form-label">Metode Pembayaran</label>
+                <select class="form-select mb-3 shadow-none">
+                    <option selected="">Tunai</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                </select>
             </div>
             <div class="form-group">
-                <label class="form-label" for="exampleInputphone">Teliphone Input</label>
-                <input type="tel" class="form-control" id="exampleInputphone" value="1-(555)-555-5555" />
+                <label class="form-label" for="exampleInputText1">Diskon Pembelian</label>
+                <input type="text" class="form-control" id="exampleInputText1" placeholder="Diskon Pembelian.." />
             </div>
             <div class="form-group">
-                <label class="form-label" for="exampleInputNumber1">Number Input</label>
-                <input type="number" class="form-control" id="exampleInputNumber1" value="2356" />
+                <label class="form-label" for="exampleInputText1">Produk yang dibeli</label>
+                <input type="text" class="form-control" id="exampleInputText1" placeholder="Produk yang dibeli.." />
             </div>
             <div class="form-group">
-                <label class="form-label" for="exampleInputPassword3">Password Input</label>
-                <input type="password" class="form-control" id="exampleInputPassword3" value="markjhon123" placeholder="Enter Password" />
+                <label class="form-label" for="exampleInputText1">Pajak</label>
+                <input type="text" class="form-control" id="exampleInputText1" placeholder="Pajak.." />
             </div>
             <div class="form-group">
-                <label class="form-label" for="exampleInputdate">Date Input</label>
-                <input type="date" class="form-control" id="exampleInputdate" value="2019-12-18" />
-            </div>
-            <div class="form-group">
-                <label class="form-label" for="exampleInputmonth">Month Input</label>
-                <input type="month" class="form-control" id="exampleInputmonth" value="2019-12" />
-            </div>
-            <div class="form-group">
-                <label class="form-label" for="exampleInputweek">Week Input</label>
-                <input type="week" class="form-control" id="exampleInputweek" value="2019-W46" />
-            </div>
-            <div class="form-group">
-                <label class="form-label" for="exampleInputtime">Time Input</label>
-                <input type="time" class="form-control" id="exampleInputtime" value="13:45" />
-            </div>
-            <div class="form-group">
-                <label class="form-label" for="exampleInputdatetime">Date and Time Input</label>
-                <input type="datetime-local" class="form-control" id="exampleInputdatetime" value="2019-12-19T13:45:00" />
-            </div>
-            <div class="form-group">
-                <label class="form-label" for="exampleFormControlTextarea1">Example textarea</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="5"></textarea>
+                <label class="form-label" for="exampleInputText1">Total Pembelian</label>
+                <input type="text" class="form-control" id="exampleInputText1" placeholder="Total Pembelian.." />
             </div>
             <button type="submit" class="btn btn-primary rounded">Submit</button>
             <button type="submit" class="btn btn-danger rounded">cancel</button>

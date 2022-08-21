df51ff1728de1d2c8657d61e26ed23caec477083
SalmaSyawalan <salma.syawalan@gmail.com>
2022-08-21 16:39:30 +0700

    update modal

--- resources/views/app/modal/tambah_modal.blade.php
+++ resources/views/app/modal/tambah_modal.blade.php
@@ -18,31 +18,8 @@
                 <label class="form-label" for="exampleInputdate">Saldo Awal (Harga Modal)</label>
                 <input type="text" class="form-control" id="exampleInputText1" placeholder="Masukkan nominal harga modal anda" />
             </div>
-            <!--<div class="form-group">
-                <label class="form-label">Metode Pembayaran</label>
-                <select class="form-select mb-3 shadow-none" disabled="">
-                    <option selected="">Tunai</option>
-                    <option value="1">Kredit</option>
-                </select>
-            </div>
-            <div class="form-group">
-                <label class="form-label" for="exampleInputText1">Diskon Pembelian</label>
-                <input type="text" class="form-control" id="exampleInputText1" placeholder="Diskon Pembelian.." />
-            </div>
-            <div class="form-group">
-                <label class="form-label" for="exampleInputText1">Produk yang dibeli</label>
-                <input type="text" class="form-control" id="exampleInputText1" placeholder="Produk yang dibeli.." />
-            </div>
-            <div class="form-group">
-                <label class="form-label" for="exampleInputText1">Pajak</label>
-                <input type="text" class="form-control" id="exampleInputText1" placeholder="Pajak.." />
-            </div>
-            <div class="form-group">
-                <label class="form-label" for="exampleInputText1">Total Pembelian</label>
-                <input type="text" class="form-control" id="exampleInputText1" placeholder="Total Pembelian.." />
-            </div>-->
             <button type="submit" class="btn btn-primary rounded">Submit</button>
-            <button type="button" class="btn btn-danger rounded" href="{{ route('modal') }}">Cancel</button>
+            <a class="btn btn-danger rounded" href="{{ url()->previous() }}">Cancel</a>
             
         </form>
     </div>

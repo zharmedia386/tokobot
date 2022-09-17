da978324d45b690c5fad86a70c21a8e8992448f9
M Azhar Alauddin <80077273+zharmedia386@users.noreply.github.com>
2022-09-17 20:10:54 +0700

    Sign In dan Sign Up Completed UI

--- resources/views/auth/sign-in.blade.php
+++ resources/views/auth/sign-in.blade.php
@@ -7,12 +7,13 @@
         <div class="row align-items-center">
             <div class="col-md-12 col-lg-7 col-xl-4">
                 <div class="card-body">
-                    <a href="{{ route('home') }}">
+                    <!-- <a href="{{ route('home') }}">
                         <img src="{{ asset('assets/images/favicon.png') }}" class="img-fluid logo-img" alt="img4" />
-                    </a>
+                    </a> -->
                     <br><br>
-                    <h2 class="mb-2 text-center">Sign In</h2>
-                    <p class="text-center">Sign in to stay connected.</p>
+                    <h2 class="mb-2 text-center">Login Akun</h2>
+                    <br>
+                    <!-- <p class="text-center">Sign in to stay connected.</p> -->
 
                     <!-- ALERT LOGIN FIRST -->
                     @if (session()->has('loginFirst'))
@@ -52,15 +53,15 @@
                             <div class="col-lg-12 d-flex justify-content-between">
                                 <div class="form-check mb-3">
                                     <input type="checkbox" class="form-check-input" id="customCheck1" />
-                                    <label class="form-check-label" for="customCheck1">Remember Me</label>
+                                    <label class="form-check-label" for="customCheck1">Ingat saya</label>
                                 </div>
-                                <a href="recoverpw.html">Forgot Password?</a>
+                                <a href="recoverpw.html">Lupa Password?</a>
                             </div>
                         </div>
                         <div class="d-flex justify-content-center">
                             <button type="submit" class="btn btn-primary">Sign In</button>
                         </div>
-                        <p class="text-center my-3">or sign in with other accounts?</p>
+                        <!-- <p class="text-center my-3">or sign in with other accounts?</p>
                         <div class="d-flex justify-content-center">
                             <ul class="list-group list-group-horizontal list-group-flush">
                                 <li class="list-group-item border-0 pb-0">
@@ -76,12 +77,12 @@
                                     <a href="#"><img src="{{ asset('assets/images/brands/li.svg') }}" alt="li" /></a>
                                 </li>
                             </ul>
-                        </div>
-                        <p class="mt-3 text-center">Don’t have an account? <a href="{{route('register')}}" class="text-underline">Click here to sign up.</a></p>
+                        </div> -->
+                        <p class="mt-3 text-center">Belum punya akun? <a href="{{route('register')}}" class="text-underline"><br><strong>Klik disini</strong></a> untuk bikin akun baru</p>
                     </form>
                 </div>
             </div>
-            <div class="col-md-12 col-lg-5 col-xl-8 d-lg-block d-none vh-100 overflow-hidden">
+            <div class="col-md-12 col-lg-5 col-xl-7 d-lg-block d-none vh-100 overflow-hidden">
                 <img src="{{ asset('assets/images/auth/09.png') }}" class="img-fluid sign-in-img" alt="images" />
             </div>
         </div>
--- resources/views/auth/sign-up.blade.php
+++ resources/views/auth/sign-up.blade.php
@@ -8,12 +8,13 @@
             <div class="col-md-12 col-lg-7 col-xl-4">
                 <div class="d-flex justify-content-center mb-0">
                     <div class="card-body mt-5">
-                        <a href="{{ route('home') }}">
+                        <!-- <a href="{{ route('home') }}">
                             <img src="{{ asset('assets/images/favicon.png') }}" class="img-fluid logo-img" alt="img5" />
-                        </a>
+                        </a> -->
                         <br><br>
-                        <h2 class="mb-2 text-center">Sign Up</h2>
-                        <p class="text-center">Create your account.</p>
+                        <h2 class="mb-2 text-center">Register Akun</h2>
+                        <br>
+                        <!-- <p class="text-center">Create your account.</p> -->
                         <form method="POST" action="{{route('register.store')}}">
                         @csrf
                             <div class="row">
@@ -53,17 +54,17 @@
                                         <input type="password" class="form-control form-control-sm" id="confirm-password" name="confirm_password" placeholder="..." />
                                     </div>
                                 </div>
-                                <div class="col-lg-12 d-flex justify-content-center">
+                                <!-- <div class="col-lg-12 d-flex justify-content-center">
                                     <div class="form-check mb-3">
                                         <input type="checkbox" class="form-check-input" id="customCheck1" />
                                         <label class="form-check-label" for="customCheck1">I agree with the terms of use</label>
                                     </div>
-                                </div>
+                                </div> -->
                             </div>
                             <div class="d-flex justify-content-center">
                                 <button type="submit" class="btn btn-primary">Sign Up</button>
                             </div>
-                            <p class="text-center my-3">or sign in with other accounts?</p>
+                            <!-- <p class="text-center my-3">or sign in with other accounts?</p>
                             <div class="d-flex justify-content-center">
                                 <ul class="list-group list-group-horizontal list-group-flush">
                                     <li class="list-group-item border-0 pb-0">
@@ -79,13 +80,13 @@
                                         <a href="#"><img src="{{ asset('assets/images/brands/li.svg') }}" alt="li" /></a>
                                     </li>
                                 </ul>
-                            </div>
-                            <p class="mt-3 text-center">Already have an Account <a href="{{ route('login') }}" class="text-underline">Sign In</a></p>
+                            </div> -->
+                            <p class="mt-3 text-center">Sudah punya akun? <a href="{{ route('login') }}" class="text-underline"><br><strong>Klik disini</strong></a> untuk Login akun</p>
                         </form>
                     </div>
                 </div>
             </div>
-            <div class="col-md-12 col-lg-5 col-xl-8 d-lg-block d-none overflow-hidden">
+            <div class="col-md-12 col-lg-5 col-xl-7 d-lg-block d-none vh-100 overflow-hidden">
                 <img src="{{ asset('assets/images/auth/09.png') }}" class="img-fluid sign-in-img" alt="images" />
             </div>
         </div>
